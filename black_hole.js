ec07835ad5149b714ac368427e952bd34dff062c
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2020-12-13 14:58:46 -0500

    delta time

--- black_hole.js
+++ black_hole.js
@@ -1,66 +1,144 @@
+/* 
+  Reference(s):
+  Aerotwist - Creating Particles with Three.js
+  https://aerotwist.com/tutorials/creating-particles-with-three-js/ 
+*/
+
+
+// need delta time to make the animation speed indipendent from frame rate
+var delta_time = 0.0; // seconds b/w each frames
+var prev_time = 0.0; // last recorded time
+
 
 class BlackHole{
 
   constructor() {
-    // black sphere
-    var geometry = new THREE.SphereGeometry( 30, 30, 30 );
-    var material = new THREE.MeshStandardMaterial({ color: 'black' });
-    this.sphere_mesh = new THREE.Mesh( geometry, material );
-
-    /* 
-      Three.js particle tutorial
-      https://aerotwist.com/tutorials/creating-particles-with-three-js/ 
-    */
-   // create particle variables
-    this.particleCount = 20000;
-    this.particles = new THREE.Geometry();
-    var pMaterial = new THREE.PointsMaterial({
-      color: 0xffffff,
-      size: 5,
+    // black sphere (event horizon)
+    const sphereGeo = new THREE.SphereGeometry( 30, 30, 30 );
+    const darkMat = new THREE.MeshStandardMaterial({ color: 'black' });
+    this.sphereMesh = new THREE.Mesh( sphereGeo, darkMat );
+    
+    // disk
+    noise.seed(Math.random());
+    this.rotateAxis = new THREE.Vector3( 0, 1, 0 );
+    this.diskStart = 50;
+    this.diskRange = 3000;
+    this.diskThickness = 10;
+    
+   // particle variables (acceleration disk)
+    this.diskParticleCount = 30000;
+    this.diskParticles = new THREE.Geometry();
+    const diskParticleMat = new THREE.PointsMaterial({
+      size: 10,
+      vertexColors: true
     });
 
-    this.y_axis = new THREE.Vector3( 0, 1, 0 );
-    this.disk_min = 50;
-    this.disk_range = 3000;
-
-    // create individual particles
-    for (var p = 0; p < this.particleCount; p++) {
-      var l = Math.random() * this.disk_range + this.disk_min
-      var d = Math.random() * 2 * Math.PI
-      var pX = l * Math.cos( d )
-      var pY = Math.random() * 10 - 5;
-      var pZ = l * Math.sin( d )
-      var particle = new THREE.Vector3( pX, pY, pZ );
-      // add this particle to the geometry
-      this.particles.vertices.push( particle );
+    // create individual disk particles
+    for (var p = 0; p < this.diskParticleCount; p++) {
+      // position
+      const radius = Math.random()*this.diskRange + this.diskStart;
+      const theta = Math.random() * 2*Math.PI;
+      const x = radius * Math.cos( theta );
+      const y = Math.random()*this.diskThickness - this.diskThickness/2;
+      const z = radius * Math.sin( theta );
+      var particle = new THREE.Vector3( x, y, z );
+      this.diskParticles.vertices.push( particle );
+      // color
+      const r = noise.perlin2( x/1000, radius/200 )/2 + 0.5;
+      const g = 0.5;
+      const b = 0.5;
+      var color = new THREE.Color( r, g, b );
+      this.diskParticles.colors.push( color );
     }
 
-    // create the particle system
-    this.particleSystem = new THREE.Points( this.particles, pMaterial );
+    this.diskParticleSystem = new THREE.Points( this.diskParticles, diskParticleMat );
+
+    // jet
+    this.isJetting = true;
+    this.jettingInterval = 10;
+    this.jetCountDown = 0;
+    this.jetNext = 0;
+
+    // particle variables (astrophysical jet)
+    this.jetParticleCount = 200;
+    this.jetParticles = new THREE.Geometry();
+    const jetParticleMat = new THREE.PointsMaterial({
+      size: 10,
+      color: 0x3030ff
+    });
+
+    // create individual jet particles
+    for (var p = 0; p < this.jetParticleCount; p++) {
+      var particle = new THREE.Vector3( 0, 0, 0 );
+      particle.jetted = false;
+      this.jetParticles.vertices.push( particle );
+    }
+
+    this.jetParticleSystem = new THREE.Points( this.jetParticles, jetParticleMat );
   }
 
   show(scene) {
-    scene.add( this.sphere_mesh );
-    scene.add( this.particleSystem );
+    scene.add( this.sphereMesh );
+    scene.add( this.diskParticleSystem );
+    scene.add( this.jetParticleSystem );
   }
 
   onAnimate() {
-    // for each particle
-    var pCount = this.particleCount;
-    while (pCount--) {
-      var particle = this.particles.vertices[pCount];
+    // update delta time
+    now_time = new Date();
+    delta_time = (now_time - prev_time) / 1000;
+    prev_time = now_time;
 
+    // for each disk particle
+    var dpCount = this.diskParticleCount;
+    while (dpCount--) {
+      var dParticle = this.diskParticles.vertices[dpCount];
       // update particle position
-      var dst = particle.distanceTo( this.sphere_mesh.position )
-      var c = 30
-      var p = 1.7
-      var d = 50
-      var angular_velocity = c / Math.pow(dst-this.disk_min+d, p)
-      particle.applyAxisAngle( this.y_axis, angular_velocity );
+      const dst = dParticle.distanceTo( this.sphereMesh.position );
+      const c = 30;
+      const p = 1.7;
+      const d = 50;
+      const angular_velocity = c / Math.pow(dst-this.diskStart+d, p);
+      dParticle.applyAxisAngle( this.rotateAxis, angular_velocity );
     }
 
     // flag to the particle system that we've changed its vertices.
-    this.particleSystem.geometry.verticesNeedUpdate = true;
+    this.diskParticleSystem.geometry.verticesNeedUpdate = true;
+
+    // launch next jet particle
+    if (this.isJetting && !this.jetCountDown--) {
+      var jParticle = this.jetParticles.vertices[this.jetNext];
+      if (jParticle.jetted) {
+        // reuse
+        jParticle.set( 0, 0, 0 );
+      }
+      else {
+        jParticle.jetted = true;
+      }
+      this.jetNext = ++this.jetNext % this.jetParticleCount
+      this.jetCountDown = this.jettingInterval;
+    }
+
+    // for each launched jet particle
+    var jpCount = this.jetParticleCount;
+    while (jpCount--) {
+      var jParticle = this.jetParticles.vertices[jpCount];
+      if (jParticle.jetted) {
+        // update particle position
+        const i = jParticle.y - 200; // y - (0 ~ 3000)
+        const f = 0.01;  // 0 ~ 0.5
+        const b = 1.2;   // 1 ~ 5
+        const s = 0.012;    // 0 ~ 0.5
+        const a = 1.3;   // 0 ~ 30
+        const yV = 0.3;
+        const xV = Math.cos( i*f ) * Math.pow(b, -s*i) * a;
+        const zV = Math.sin( i*f ) * Math.pow(b, -s*i) * a;
+        jParticle.add(new THREE.Vector3( xV, yV, zV));
+      }
+    }
+
+    // flag to the particle system that we've changed its vertices.
+    this.jetParticleSystem.geometry.verticesNeedUpdate = true;
   }
 
 }
