48e072b0ea3cec9c4921ea66ac36c08c8591934b
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2020-12-13 21:36:14 -0500

    jets both directoin and rotates

--- black_hole.js
+++ black_hole.js
@@ -5,25 +5,21 @@
 */
 
 
-// need delta time to make the animation speed indipendent from frame rate
-var delta_time = 0.0; // seconds b/w each frames
-var prev_time = 0.0; // last recorded time
-
-
 class BlackHole{
 
   constructor() {
     // black sphere (event horizon)
-    const sphereGeo = new THREE.SphereGeometry( 30, 30, 30 );
+    const sphereGeo = new THREE.SphereGeometry( 90, 90, 90 );
     const darkMat = new THREE.MeshStandardMaterial({ color: 'black' });
     this.sphereMesh = new THREE.Mesh( sphereGeo, darkMat );
     
+    // =========================================================================
     // disk
     noise.seed(Math.random());
     this.rotateAxis = new THREE.Vector3( 0, 1, 0 );
-    this.diskStart = 50;
-    this.diskRange = 3000;
-    this.diskThickness = 10;
+    this.diskStart = 100;
+    this.diskRange = 1000;
+    this.diskThickness = 30;
     
    // particle variables (acceleration disk)
     this.diskParticleCount = 30000;
@@ -53,14 +49,16 @@ class BlackHole{
 
     this.diskParticleSystem = new THREE.Points( this.diskParticles, diskParticleMat );
 
+    // =========================================================================
     // jet
     this.isJetting = true;
-    this.jettingInterval = 10;
+    this.jettingInterval = 0;
     this.jetCountDown = 0;
     this.jetNext = 0;
+    this.jetDirection = 1; // 1 / -1
 
     // particle variables (astrophysical jet)
-    this.jetParticleCount = 200;
+    this.jetParticleCount = 30000;
     this.jetParticles = new THREE.Geometry();
     const jetParticleMat = new THREE.PointsMaterial({
       size: 10,
@@ -70,6 +68,7 @@ class BlackHole{
     // create individual jet particles
     for (var p = 0; p < this.jetParticleCount; p++) {
       var particle = new THREE.Vector3( 0, 0, 0 );
+      particle.velocity = new THREE.Vector3( 0, 0, 0 );
       particle.jetted = false;
       this.jetParticles.vertices.push( particle );
     }
@@ -84,11 +83,6 @@ class BlackHole{
   }
 
   onAnimate() {
-    // update delta time
-    now_time = new Date();
-    delta_time = (now_time - prev_time) / 1000;
-    prev_time = now_time;
-
     // for each disk particle
     var dpCount = this.diskParticleCount;
     while (dpCount--) {
@@ -105,16 +99,26 @@ class BlackHole{
     // flag to the particle system that we've changed its vertices.
     this.diskParticleSystem.geometry.verticesNeedUpdate = true;
 
+    // =========================================================================
     // launch next jet particle
     if (this.isJetting && !this.jetCountDown--) {
       var jParticle = this.jetParticles.vertices[this.jetNext];
       if (jParticle.jetted) {
         // reuse
         jParticle.set( 0, 0, 0 );
+        jParticle.velocity.set( 0, 0, 0 );
       }
       else {
         jParticle.jetted = true;
       }
+      // set velocity
+      const radius = Math.random();
+      const theta = Math.random() * 2*Math.PI;
+      const x = radius * Math.cos( theta );
+      const y = 8 * (this.jetDirection *= -1);
+      const z = radius * Math.sin( theta );
+      jParticle.velocity.set( x, y, z );
+      // get ready for next one
       this.jetNext = ++this.jetNext % this.jetParticleCount
       this.jetCountDown = this.jettingInterval;
     }
@@ -125,15 +129,10 @@ class BlackHole{
       var jParticle = this.jetParticles.vertices[jpCount];
       if (jParticle.jetted) {
         // update particle position
-        const i = jParticle.y - 200; // y - (0 ~ 3000)
-        const f = 0.01;  // 0 ~ 0.5
-        const b = 1.2;   // 1 ~ 5
-        const s = 0.012;    // 0 ~ 0.5
-        const a = 1.3;   // 0 ~ 30
-        const yV = 0.3;
-        const xV = Math.cos( i*f ) * Math.pow(b, -s*i) * a;
-        const zV = Math.sin( i*f ) * Math.pow(b, -s*i) * a;
-        jParticle.add(new THREE.Vector3( xV, yV, zV));
+        const angular_velocity = 0.08;
+        jParticle.applyAxisAngle( this.rotateAxis, angular_velocity );
+        jParticle.velocity.applyAxisAngle( this.rotateAxis, angular_velocity );
+        jParticle.add(jParticle.velocity);
       }
     }
 
