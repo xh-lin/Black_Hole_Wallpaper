d7cb96f6e62d661ee40b6986e07f632168c32483
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2020-12-19 15:28:46 -0500

    housekeeping

--- black_hole.js
+++ black_hole.js
@@ -1,13 +1,34 @@
-/* 
-  Reference(s):
-  Aerotwist - Creating Particles with Three.js
-  https://aerotwist.com/tutorials/creating-particles-with-three-js/
-  Advanced: Web audio visualizer
-  https://steamcommunity.com/sharedfiles/filedetails/?id=786006047
-*/
+/**
+ * Reference(s):
+ * Aerotwist - Creating Particles with Three.js
+ * https://aerotwist.com/tutorials/creating-particles-with-three-js
+ * Advanced: Web audio visualizer
+ * https://steamcommunity.com/sharedfiles/filedetails/?id=786006047
+ */
+
+
+/* things for audio processing */
+
+// An array containing audio data for the left and right channels
+// left channel: [0:63], right channel: [64:127]
+// low frequencies are near the beginning, high ones at the end of the array
+// containing 128 positive floats which may go beyond 1.0
+let audio = [];
+// this will be called by the audio listener to update the audio array
+const listener = arr => {
+  audio = arr;
+}
+// containing indices of disk particles for 128 audio buckets
+let diskFrequencies = [];
+for (let i = 0; i < 128; i++)
+  diskFrequencies.push( [] );
+
+
+/* black hole */
 
 // get color in between from color A to B
-// alpha: [0, 1]
+// colorA, colorB: THREE.Color
+// alpha: a float from 0 to 1
 colorBetween = function(colorA, colorB, alpha) {
   const dR = colorB.r - colorA.r;
   const dG = colorB.g - colorA.g;
@@ -15,34 +36,33 @@ colorBetween = function(colorA, colorB, alpha) {
   return new THREE.Color( colorA.r+dR*alpha, colorA.g+dG*alpha, colorA.b+dB*alpha );
 };
 
-// audio array: 128 elements include each elements are float from 0 to 1
-let audio = [];
-const listener = arr => {
-  audio = arr;
-}
-// disk Frequency
-let diskFrequencies = [];
-for (var i = 0; i < 128; i++){
-  diskFrequencies.push([]);
-}
 
 class BlackHole{
   
+  /* sphereRadius, diskColors, diskStart, diskRange, diskThickness, 
+    diskParticleCount, diskParticleSize, diskTexture, 
+    jetEnabled, jettingInterval,
+    jetParticleCount, jetParticleSize, jetTexture,
+    starDstStart, starDstRange,
+    starParticleCount, starSize, starTexture, */
   constructor() {
 
-    var colors = [];
-    colors.push(new THREE.Color('lightyellow'));
-    colors.push(new THREE.Color('orange'));
-    colors.push(new THREE.Color('violet'));
-    colors.push(new THREE.Color('blue'));
-
     // black sphere (event horizon)
-    const sphereGeo = new THREE.SphereGeometry( 60, 60, 60 );
+    this.sphereRadius = 90;
+    const sphereGeo = new THREE.SphereGeometry( this.sphereRadius, this.sphereRadius, this.sphereRadius );
     const darkMat = new THREE.MeshStandardMaterial({ color: 'black' });
     this.sphereMesh = new THREE.Mesh( sphereGeo, darkMat );
     
     // =========================================================================
     // disk
+
+    const diskColors = [
+      new THREE.Color( 'lightyellow' ),
+      new THREE.Color( 'orange' ),
+      new THREE.Color( 'violet' ),
+      new THREE.Color( 'blue' )
+    ];
+
     this.rotateAxis = new THREE.Vector3( 0, 1, 0 );
     this.diskStart = 100;
     this.diskRange = 1000;
@@ -54,69 +74,71 @@ class BlackHole{
     const diskParticleMat = new THREE.PointsMaterial({
       vertexColors: true,
       size: 20,
-      map: new THREE.TextureLoader().load(
-        "spark1.png"
-      ),
+      map: new THREE.TextureLoader().load( "spark1.png" ),
       blending: THREE.AdditiveBlending,
       transparent: true,
       depthWrite: false
     });
 
     // create individual disk particles
-    for (var p = 0; p < this.diskParticleCount; p++) {
+    for (let p = 0; p < this.diskParticleCount; p++) {
+
       // position
       const theta = Math.random() * 2*Math.PI;
       const radius = Math.random()*this.diskRange + this.diskStart;
       const x = radius * Math.cos( theta );
       const y = Math.random()*this.diskThickness - this.diskThickness/2;
       const z = radius * Math.sin( theta );
-      var particle = new THREE.Vector3( x, y, z ); 
+      let particle = new THREE.Vector3( x, y, z ); 
       this.diskParticles.vertices.push( particle );
+
       // color
-      if (radius < 400) {
-        const a = (radius - this.diskStart) / 300;
-        this.diskParticles.colors.push( colorBetween(colors[0], colors[1], a) );
-      } else if (radius < 700) {
-        const a = (radius - 400) / 300;
-        this.diskParticles.colors.push( colorBetween(colors[1], colors[2], a) );
+      const segLen = (this.diskRange - 100) / (diskColors.length - 1);
+      const zeroedR = radius - this.diskStart;
+      if (zeroedR < segLen) {
+        const alpha = zeroedR / segLen;
+        this.diskParticles.colors.push( colorBetween(diskColors[0], diskColors[1], alpha) );
+      } else if (zeroedR < 2*segLen) {
+        const alpha = (zeroedR - segLen) / segLen;
+        this.diskParticles.colors.push( colorBetween(diskColors[1], diskColors[2], alpha) );
       } else {
-        const a = (radius - 700) / 300;
-        this.diskParticles.colors.push( colorBetween(colors[2], colors[3], a) );
+        const alpha = (zeroedR - 2*segLen) / segLen;
+        this.diskParticles.colors.push( colorBetween(diskColors[2], diskColors[3], alpha) );
       }
-      // audio
-      particle.InitialValY = y; 
-      var newIndec = Math.floor(radius * 128 / 1200)
-      diskFrequencies[newIndec].push(p);
+
+      // sort particles for audio visualizer based on distance from the center
+      particle.initY = y; 
+      diskFrequencies[Math.floor( zeroedR / this.diskRange * 128 )].push(p);
+
     }
 
     this.diskParticleSystem = new THREE.Points( this.diskParticles, diskParticleMat );
 
     // =========================================================================
     // jet
-    this.isJetting = true;
+
+    this.jetEnabled = true;
     this.jettingInterval = 1;
-    this.jetCountDown = 0;
-    this.jetMult = 0;
-    this.jetNext = 0;
-    this.jetDirection = 1; // 1 / -1
+    this.jetCountDown = 0; // don't change, it's for jettingInterval
+    this.jetAmount = 0; // don't change, amount of particles to launch every time
+    this.jetNext = 0; // don't change, id of next particle to launch
+    this.jetDirection = 1; // don't change, for launching particles in both directions
 
     // particle variables (astrophysical jet)
-    this.jetParticleCount = 30000;
+    this.jetParticleCount = 5000;
     this.jetParticles = new THREE.Geometry();
     const jetParticleMat = new THREE.PointsMaterial({
       size: 60,
       color: 'lightskyblue',
-      map: new THREE.TextureLoader().load(
-        "spark1.png"
-      ),
+      map: new THREE.TextureLoader().load( "spark1.png" ),
       blending: THREE.AdditiveBlending,
       transparent: true,
       depthWrite: false
     });
 
     // create individual jet particles
-    for (var p = 0; p < this.jetParticleCount; p++) {
-      var particle = new THREE.Vector3( 0, 0, 0 );
+    for (let p = 0; p < this.jetParticleCount; p++) {
+      let particle = new THREE.Vector3( 0, 0, 0 );
       particle.velocity = new THREE.Vector3( 0, 0, 0 );
       particle.jetted = false;
       this.jetParticles.vertices.push( particle );
@@ -126,87 +148,98 @@ class BlackHole{
 
     // =========================================================================
     // stars
-    const starCount = 1000;
+
+    const starDstStart = 10000;
+    const startDstRange = 100000;
+    
+    const starParticleCount = 1000;
     this.starParticles = new THREE.Geometry();
     const starParticleMat = new THREE.PointsMaterial({
       vertexColors: true,
       size: 200,
-      map: new THREE.TextureLoader().load(
-        "spark1.png"
-      ),
+      map: new THREE.TextureLoader().load( "spark1.png" ),
       blending: THREE.AdditiveBlending,
       transparent: true,
       depthWrite: false
     });
 
-    for ( var i = 0; i < starCount; i++) {
+    for ( let i = 0; i < starParticleCount; i++) {
       // position
       const theta = Math.random() * 2*Math.PI;
       const phi = Math.random() * 2*Math.PI;
-      const radius = 10000 + Math.random()*100000;
+      const radius = starDstStart + Math.random()*startDstRange;
       const x = radius * Math.sin( theta ) * Math.cos( phi );
       const y = radius * Math.sin( theta ) * Math.sin( phi );
       const z = radius * Math.cos( theta );
-      var particle = new THREE.Vector3( x, y, z );
+      let particle = new THREE.Vector3( x, y, z );
       this.starParticles.vertices.push( particle );
       this.starParticles.colors.push( new THREE.Color( Math.random()*0xffffff ) );
     }
 
-    this.stars = new THREE.Points( this.starParticles, starParticleMat );
+    this.starParticleSystem = new THREE.Points( this.starParticles, starParticleMat );
+
   }
 
   show(scene) {
     scene.add( this.sphereMesh );
     scene.add( this.diskParticleSystem );
     scene.add( this.jetParticleSystem );
-    scene.add( this.stars );
+    scene.add( this.starParticleSystem );
   }
 
   onAnimate() {
-    // audio listener for wall paper enigne
+
+    // audio listener for wall paper enigne, updates the audio arrary
     window.wallpaperRegisterAudioListener(listener);
+
+    // =========================================================================
+    // disk
+
     // for each disk particle
-    var dpCount = this.diskParticleCount;
+    const c = 30; // overall speed
+    const p = 1.7; // how much faster when closer to the center
+    const d = 50; // shift the distance for speed calculation
+    let dpCount = this.diskParticleCount;
     while (dpCount--) {
-      var dParticle = this.diskParticles.vertices[dpCount];
-      // update particle position
-      const dst = dParticle.distanceTo( this.sphereMesh.position );
-      const c = 30;
-      const p = 1.7;
-      const d = 50;
+      // revolve around the center, closer to the center will be faster
+      let dParticle = this.diskParticles.vertices[dpCount];
+      const dst = dParticle.distanceTo( this.sphereMesh.position ); // distance from the center
       const angular_velocity = c / Math.pow(dst-this.diskStart+d, p);
       dParticle.applyAxisAngle( this.rotateAxis, angular_velocity );
     }
+
     // audio visualizer Animation for disk
-    var max = 0;
+    const strength = 60;
+    let max = 0;
     for (const [i, part] of audio.entries()) {
-      var dFreq = diskFrequencies[i];
-      for (var j = 0; j < dFreq.length; j++) {
-        this.diskParticles.vertices[dFreq[j]].y = dParticle.InitialValY + audio[i] * 60;
+      let dFreqs = diskFrequencies[i];
+      for (const dFreq of dFreqs) {
+        let dParticle = this.diskParticles.vertices[dFreq];
+        dParticle.y = dParticle.initY + audio[i] * strength;
       }
-      if(( i == 64 || i == 0 ) && max < part){
+      // get max of left and right channels
+      if((i == 64 || i == 0) && max < part) 
         max = part;
-      }
     }
     // audio visualizer Animation for jet
-    this.jetMult = 30 * max + 0.1;
+    const jetMult = 30;
+    const clampVal = 1;
+    this.jetAmount = jetMult*Math.min(clampVal, max) + 1;
     
     // flag to the particle system that we've changed its vertices.
     this.diskParticleSystem.geometry.verticesNeedUpdate = true;
 
     // =========================================================================
-    // launch next jet particle
-    if (this.isJetting && !this.jetCountDown--) {
-      for (var i = 0; i < this.jetMult; i++) {
-        var jParticle = this.jetParticles.vertices[this.jetNext];
-        if (jParticle.jetted) {
-          // reuse
-          jParticle.set( 0, 0, 0 );
-          jParticle.velocity.set( 0, 0, 0 );
-        }
-        else {
+    // jet
+
+    if (this.jetEnabled && !this.jetCountDown--) {
+      while (this.jetAmount-- > 0) {
+        // launch next jet particle
+        let jParticle = this.jetParticles.vertices[this.jetNext];
+        if (jParticle.jetted)
+          jParticle.set( 0, 0, 0 ); // reuse
+        else
           jParticle.jetted = true;
-        }
         // set velocity
         const radius = Math.random();
         const theta = Math.random() * 2*Math.PI;
@@ -215,15 +248,15 @@ class BlackHole{
         const z = radius * Math.sin( theta );
         jParticle.velocity.set( x, y, z );
         // get ready for next one
-        this.jetNext = ++this.jetNext % this.jetParticleCount
+        this.jetNext = (this.jetNext+1) % this.jetParticleCount
         this.jetCountDown = this.jettingInterval;
       }
     }
 
     // for each launched jet particle
-    var jpCount = this.jetParticleCount;
+    let jpCount = this.jetParticleCount;
     while (jpCount--) {
-      var jParticle = this.jetParticles.vertices[jpCount];
+      let jParticle = this.jetParticles.vertices[jpCount];
       if (jParticle.jetted) {
         // update particle position
         const angular_velocity = 0.08;
@@ -232,8 +265,10 @@ class BlackHole{
         jParticle.add(jParticle.velocity);
       }
     }
+
     // flag to the particle system that we've changed its vertices.
     this.jetParticleSystem.geometry.verticesNeedUpdate = true;
+
   }
 
 }
