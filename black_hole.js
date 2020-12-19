74b0cf8cc8c384c83bd0f70a85c58c1938025d8f
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2020-12-19 16:33:54 -0500

    apply delta time

--- black_hole.js
+++ black_hole.js
@@ -7,7 +7,7 @@
  */
 
 
-/* things for audio processing */
+/* things for the audio visualizer */
 
 // An array containing audio data for the left and right channels
 // left channel: [0:63], right channel: [64:127]
@@ -24,8 +24,6 @@ for (let i = 0; i < 128; i++)
   diskFrequencies.push( [] );
 
 
-/* black hole */
-
 // get color in between from color A to B
 // colorA, colorB: THREE.Color
 // alpha: a float from 0 to 1
@@ -37,19 +35,23 @@ colorBetween = function(colorA, colorB, alpha) {
 };
 
 
+// need delta time to make the animation speed indipendent from frame rate
+var deltaTime = 0.0; // seconds b/w each frames
+var prevTime = 0.0; // last recorded time
+
+function updateDeltaTime() {
+  nowTime = new Date();
+  deltaTime = (nowTime - prevTime) / 1000;
+  prevTime = nowTime;
+}
+
+
 class BlackHole{
   
-  /* sphereRadius, diskColors, diskStart, diskRange, diskThickness, 
-    diskParticleCount, diskParticleSize, diskTexture, 
-    jetEnabled, jettingInterval,
-    jetParticleCount, jetParticleSize, jetTexture,
-    starDstStart, starDstRange,
-    starParticleCount, starSize, starTexture, */
   constructor() {
 
     // black sphere (event horizon)
-    this.sphereRadius = 90;
-    const sphereGeo = new THREE.SphereGeometry( this.sphereRadius, this.sphereRadius, this.sphereRadius );
+    const sphereGeo = new THREE.SphereGeometry( 90, 20, 20 );
     const darkMat = new THREE.MeshStandardMaterial({ color: 'black' });
     this.sphereMesh = new THREE.Mesh( sphereGeo, darkMat );
     
@@ -63,7 +65,7 @@ class BlackHole{
       new THREE.Color( 'blue' )
     ];
 
-    this.rotateAxis = new THREE.Vector3( 0, 1, 0 );
+    this.revolveAxis = new THREE.Vector3( 0, 1, 0 );
     this.diskStart = 100;
     this.diskRange = 1000;
     this.diskThickness = 30;
@@ -189,27 +191,14 @@ class BlackHole{
 
   onAnimate() {
 
+    // update delta time
+    updateDeltaTime();
+
     // audio listener for wall paper enigne, updates the audio arrary
     window.wallpaperRegisterAudioListener(listener);
 
-    // =========================================================================
-    // disk
-
-    // for each disk particle
-    const c = 30; // overall speed
-    const p = 1.7; // how much faster when closer to the center
-    const d = 50; // shift the distance for speed calculation
-    let dpCount = this.diskParticleCount;
-    while (dpCount--) {
-      // revolve around the center, closer to the center will be faster
-      let dParticle = this.diskParticles.vertices[dpCount];
-      const dst = dParticle.distanceTo( this.sphereMesh.position ); // distance from the center
-      const angular_velocity = c / Math.pow(dst-this.diskStart+d, p);
-      dParticle.applyAxisAngle( this.rotateAxis, angular_velocity );
-    }
-
     // audio visualizer Animation for disk
-    const strength = 60;
+    const strength = 40;
     let max = 0;
     for (const [i, part] of audio.entries()) {
       let dFreqs = diskFrequencies[i];
@@ -221,10 +210,27 @@ class BlackHole{
       if((i == 64 || i == 0) && max < part) 
         max = part;
     }
+
     // audio visualizer Animation for jet
     const jetMult = 30;
     const clampVal = 1;
     this.jetAmount = jetMult*Math.min(clampVal, max) + 1;
+
+    // =========================================================================
+    // disk
+
+    // for each disk particle
+    const c = 2000; // overall revolving speed
+    const p = 1.7; // how much faster when closer to the center
+    const d = 50; // shift the distance from center for speed calculation
+    let dpCount = this.diskParticleCount;
+    while (dpCount--) {
+      // revolve around the center, closer to the center will be faster
+      let dParticle = this.diskParticles.vertices[dpCount];
+      const dst = dParticle.distanceTo( this.sphereMesh.position ); // distance from the center
+      const angularVelocity = c / Math.pow(dst-this.diskStart+d, p);
+      dParticle.applyAxisAngle( this.revolveAxis, angularVelocity*deltaTime );
+    }
     
     // flag to the particle system that we've changed its vertices.
     this.diskParticleSystem.geometry.verticesNeedUpdate = true;
@@ -232,6 +238,8 @@ class BlackHole{
     // =========================================================================
     // jet
 
+    const verticalVelocity = 6;
+    const scalar = 100;
     if (this.jetEnabled && !this.jetCountDown--) {
       while (this.jetAmount-- > 0) {
         // launch next jet particle
@@ -244,9 +252,9 @@ class BlackHole{
         const radius = Math.random();
         const theta = Math.random() * 2*Math.PI;
         const x = radius * Math.cos( theta );
-        const y = 8 * (this.jetDirection *= -1);
+        const y = verticalVelocity * (this.jetDirection *= -1); // flip the direction
         const z = radius * Math.sin( theta );
-        jParticle.velocity.set( x, y, z );
+        jParticle.velocity.set( x*scalar, y*scalar, z*scalar );
         // get ready for next one
         this.jetNext = (this.jetNext+1) % this.jetParticleCount
         this.jetCountDown = this.jettingInterval;
@@ -254,15 +262,15 @@ class BlackHole{
     }
 
     // for each launched jet particle
+    const angularVelocity = 5;
     let jpCount = this.jetParticleCount;
     while (jpCount--) {
       let jParticle = this.jetParticles.vertices[jpCount];
       if (jParticle.jetted) {
         // update particle position
-        const angular_velocity = 0.08;
-        jParticle.applyAxisAngle( this.rotateAxis, angular_velocity );
-        jParticle.velocity.applyAxisAngle( this.rotateAxis, angular_velocity );
-        jParticle.add(jParticle.velocity);
+        jParticle.applyAxisAngle( this.revolveAxis, angularVelocity * deltaTime );
+        jParticle.velocity.applyAxisAngle( this.revolveAxis, angularVelocity * deltaTime );
+        jParticle.add( jParticle.velocity.clone().multiplyScalar( deltaTime ) );
       }
     }
 
